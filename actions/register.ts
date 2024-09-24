b2980a734bf6aa900b6f60db23838e8e0437d61c
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-09-24 07:46:36 +0100

    register validation done

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,12 @@
 "use server";
 
-import { RegisterSchema } from "@schemas";
 import { z } from "zod";
+import { RegisterSchema } from "@schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedField = RegisterSchema.safeParse(values);
 
   if (!validatedField.success) { 
-    // return { error: validatedField.error.errors[0].message };
     return { error: "Invalidated fields!" };
   }
 
